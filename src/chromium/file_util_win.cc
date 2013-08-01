d110b42dcec63c58f84c42b5cf8c9555feb1aefe
Steven McCoy <steven.mccoy@thomsonreuters.com>
2013-08-01 14:52:48 -0400

    Add PathExists API for Windows builds.

--- src/chromium/file_util_win.cc
+++ src/chromium/file_util_win.cc
@@ -6,6 +6,16 @@
 
 #include <share.h>
 
+namespace chromium {
+
+bool PathExists(const std::string& path) {
+  return (GetFileAttributes(path.value().c_str()) != INVALID_FILE_ATTRIBUTES);
+}
+
+}  // namespace chromium
+
+// -----------------------------------------------------------------------------
+
 namespace file_util {
 
 FILE* OpenFile(const std::string& filename, const char* mode) {
